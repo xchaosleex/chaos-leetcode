47c2bcaf721928cff03abab27364354f3354d928
xchaosleex <xchaosleex@gmail.com>
2023-05-29 21:52:46 +0800

    explore DP

---
+++ alg-py/lcs.py
@@ -0,0 +1,34 @@
+def lcs(X , Y):
+    m = len(X)
+    n = len(Y)
+ 
+    dp = [[None]*(n+1) for i in range(m+1)]
+ 
+    for i in range(m+1):
+        for j in range(n+1):
+            if i == 0 or j == 0 :
+                dp[i][j] = 0
+            elif X[i-1] == Y[j-1]:
+                dp[i][j] = dp[i-1][j-1]+1
+            else:
+                dp[i][j] = max(dp[i-1][j] , dp[i][j-1])
+
+    lcs_str = ""
+    i = m
+    j = n
+
+    while i > 0 and j > 0:
+        if X[i-1] == Y[j-1]:
+            lcs_str = X[i-1] + lcs_str
+            i -= 1
+            j -= 1
+        elif dp[i-1][j] > dp[i][j-1]:
+            i -= 1
+        else:
+            j -= 1
+
+    return dp[m][n], lcs_str
+ 
+X = "ABCBDAB"
+Y = "BDCAB"
+print("Length of LCS and LCS are ", lcs(X, Y))
