20b9290c2029f46f7278559cc43197875c2aa2c0
lizhichao <lizhichao@shanshu.ai>
2023-05-30 11:16:24 +0800

    normally commit

--- alg-py/lcs.py
+++ alg-py/lcs.py
@@ -1,21 +1,21 @@
-def lcs(X , Y):
+def lcs(X, Y):
     m = len(X)
     n = len(Y)
- 
+
     dp = [[None]*(n+1) for i in range(m+1)]
- 
+
     for i in range(m+1):
         for j in range(n+1):
-            if i == 0 or j == 0 :
+            if i == 0 or j == 0:
                 dp[i][j] = 0
             elif X[i-1] == Y[j-1]:
                 dp[i][j] = dp[i-1][j-1]+1
             else:
-                dp[i][j] = max(dp[i-1][j] , dp[i][j-1])
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1])
 
     lcs_str = ""
-    i = m
-    j = n
+    i = m  # 1
+    j = n  # 0
 
     while i > 0 and j > 0:
         if X[i-1] == Y[j-1]:
@@ -28,7 +28,8 @@ def lcs(X , Y):
             j -= 1
 
     return dp[m][n], lcs_str
- 
+
+
 X = "ABCBDAB"
 Y = "BDCAB"
 print("Length of LCS and LCS are ", lcs(X, Y))
