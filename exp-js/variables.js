73fd5324a08fc56f30ffeab563b1c07ba9ab6e0d
xchaosleex <xchaosleex@gmail.com>
2023-06-07 07:54:36 +0800

    explore more

--- exp-js/variables.js
+++ exp-js/variables.js
@@ -12,6 +12,7 @@ function example() {
     let x = 10;
     if (true) {
         let x = 20; // creates a new x variable in the block scope
+        // let x = 10; // SyntaxError: Identifier 'x' has already been declared
         console.log(x); // output: 20
     }
     console.log(x); // output: 10
@@ -23,6 +24,8 @@ function example() {
     const x = 10;
     if (true) {
         const x = 20; // creates a new x variable in the block scope
+        // const x = 10; // SyntaxError: Identifier 'x' has already been declared
+        // x = 20; // TypeError: Assignment to constant variable.
         console.log(x); // output: 20
     }
     console.log(x); // output: 10
